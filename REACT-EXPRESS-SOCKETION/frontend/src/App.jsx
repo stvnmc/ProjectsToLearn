e96f88c3d1e109ab2d768bb44706167f220d2d43
stvnmc <stvnmc123@gmail.com>
2023-09-21 18:24:36 -0600

    Add new proyect REACT-EXPRESS-SOCKERTION

--- REACT-EXPRESS-SOCKETION/frontend/src/App.jsx
+++ REACT-EXPRESS-SOCKETION/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 
 const socket = io("/");
@@ -6,6 +6,7 @@ const socket = io("/");
 function App() {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const messageListRef = useRef(null);
 
   const handleSubtmit = (e) => {
     e.preventDefault();
@@ -15,6 +16,10 @@ function App() {
     };
     setMessages([...messages, newMessage]);
     socket.emit("message", message);
+    setTimeout(() => {
+      messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
+    }, 50);
+    setMessage("");
   };
 
   useEffect(() => {
@@ -29,23 +34,27 @@ function App() {
     setMessages((state) => [...state, message]);
 
   return (
-    <div>
+    <div className="app">
       <form onSubmit={handleSubtmit}>
-        <input
-          type="text"
-          placeholder="Write your message ..."
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button>Send</button>
+        <h2>Chat React Socketion</h2>
+        <ul ref={messageListRef}>
+          {messages.map((message, i) => (
+            <li key={i} className={message.from == "Me" ? "me" : "other"}>
+              <span>{message.from} :</span>
+              <p>{message.body}</p>
+            </li>
+          ))}
+        </ul>
+        <div>
+          <input
+            type="text"
+            value={message} 
+            placeholder="  Write your message ..."
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button>Send</button>
+        </div>
       </form>
-
-      <ul>
-        {messages.map((message, i) => (
-          <li key={i}>
-            {message.from}:{message.body}
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
